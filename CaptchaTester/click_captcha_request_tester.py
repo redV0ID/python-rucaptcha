af2c5479bbc1e71c0a62fa96c2befdee47541aa2
redV0ID <thousandkats@gmail.com>
2017-08-12 20:31:17 +0700

    minor fixes

--- CaptchaTester/click_captcha_request_tester.py
+++ CaptchaTester/click_captcha_request_tester.py
@@ -1,10 +1,16 @@
 import requests
 import ClickCaptcha
 
-#pass your api key
-RUCAPTCHA_KEY = ''
+"""
+Этот пример показывает то как нужно работать с модулем для распознования обычной капчи,
+на примере нашего сайта.
+"""
+
+#pass your api key from your RuCaptcha Account
+RUCAPTCHA_KEY = ""
 #pass link to recaptcha image as parameter
-clickcaptcha_link = 'http://85.255.8.26/{0}'.format("")
+link = ""
+clickcaptcha_link = "http://85.255.8.26/{0}".format(link)
 #create an object and let it do the wordk
 click_captcha = ClickCaptcha(RUCAPTCHA_KEY)
 coordinates = click_captcha.captcha_handler(clickcaptcha_link)
